c08ff1b16b7524746c7f32bfe05d3df2b633fbee
Skellent <skellent69@gmail.com>
2025-07-06 18:47:14 -0400

    Se creo un .exe ejecutable de ADO, unicamente hay que hacer una IP estatica para que no pierda funcionalidad, se creo la navbar del login

---
+++ src/modules/ayuda.py
@@ -0,0 +1,14 @@
+# Modulo de Pagina de Ayuda
+
+# Importacion de Librerias
+import streamlit as st
+from streamlit_option_menu import option_menu as stmenu
+
+# Importacion de Constantes
+from constantes import TITULO_LOGIN
+
+# Importacion de Modulos
+from time import sleep
+
+def pagina_ayuda():
+    st.title("Seccion de Ayuda y Preguntas")
